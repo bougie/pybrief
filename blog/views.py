40ce04136d7e8c77826411f72b72a3a883e0c8e7
bougie <bougie@appartland.eu>
2014-11-15 18:49:45 +0100

    feat(blog): reversed list of blog posts in the blog list page

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ from .models import Post
 def index(request):
     """Blog home page. Render the posts list"""
 
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-create_date')
 
     return render_to_response('blog/index.tpl', {'posts': posts})
