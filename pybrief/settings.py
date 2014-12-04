eb0434a936fdb3cc13ea5cb5dcfb9641a327753a
bougie <bougie@appartland.eu>
2014-12-04 18:47:25 +0100

    fix(blog): LastCommit settings items

--- pybrief/settings.py
+++ pybrief/settings.py
@@ -86,3 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+LOG_DIR = '/tmp'
+PID_DIR = '/tmp'
