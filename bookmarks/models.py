d6ec6c1dd156b006cf79b5b1b8b707cc5a7d0390
bougie <bougie@appartland.eu>
2014-12-31 17:03:44 +0100

    feat(bookmarks): get page title when saving or editing new link

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from core.models import Tag
+from .utils import get_title_link
 
 
 class Link(models.Model):
     """Bookmark link representation"""
 
     name = models.CharField(max_length=255, blank=True, null=True)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     url = models.TextField()
     insert_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(Tag, null=True)
+
+    def save(self, *args, **kwargs):
+        self.title = get_title_link(url=self.url)
+
+        super(Link, self).save(args, kwargs)
---
+++ bookmarks/utils.py
@@ -0,0 +1,45 @@
+import re
+import urllib.request
+import html.parser
+
+
+def get_title_link(url):
+    """Get the page title for a given URL
+
+    :param url: url you which get the title
+    :type url: str"""
+
+    title = None
+
+    try:
+        req = urllib.request.Request(url)
+        req.add_header('User-agent', 'Mozilla/5.0')
+
+        response = urllib.request.urlopen(req)
+        try:
+            response_charset = response.getheader(
+                'Content-Type').split(';')[1].split('=')[1]
+        except:
+            # Charset does not exists in the Content-Type header
+            # Using UTF-8 charset by default
+            response_charset = 'UTF-8'
+        content = str(response.read(), response_charset)
+
+        tpattern = re.search(
+            '<head.*>.*<title>(.*)</title>.*</head>',
+            content,
+            flags=re.IGNORECASE | re.DOTALL
+        )
+
+        if tpattern is not None:
+            # Remove useless spaces and convert HTML entities
+            # into human readable ones
+            ti = map(lambda s: s.translate(s.maketrans("\n\t\r", "   ")),
+                     tpattern.group(1).split("\n"))
+            h = html.parser.HTMLParser()
+
+            title = h.unescape(" ".join(ti).strip())
+    except Exception:
+        title = None
+    finally:
+        return title
